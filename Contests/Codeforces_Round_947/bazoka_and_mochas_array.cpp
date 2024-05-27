11f86800ee99906118c1d212f3927a5961ebb179
Swayam Takkamore <70209059+SwayamTakkamore@users.noreply.github.com>
2024-05-27 12:45:57 +0530

    added bazoka

---
+++ Contests/Codeforces_Round_947/bazoka_and_mochas_array.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t,i;
+    for(i=0;i<t;i++){
+        int n,j;
+        cin>>n;
+        int a[n];
+        for(j=0;j<n;j++){
+            cin>>a[j];
+        }
+    }
+}
